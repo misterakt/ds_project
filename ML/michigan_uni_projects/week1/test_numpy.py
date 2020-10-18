9564dc499201dc66d1a5b797e50b82215c3bf2cf
kwangtaek <kwangtaek@element.in>
2020-10-18 17:34:52 +0200

    upload michigan_uni_ML_projects

---
+++ ML/michigan_uni_projects/week1/test_numpy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 10:42:25 2020
+
+@author: kwangtaek
+"""
+import numpy as np
+
+
+data = [[4,5.4,1],[0,9,8.1]]
+
+arr = np.array(data)
+
+arr.shape
+
+
+np.arange(19)
